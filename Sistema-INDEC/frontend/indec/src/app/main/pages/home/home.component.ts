133ee1e84873570ff79c782d68770986c054c269
Roberto Gomez <rgomezmartearena@ubp.edu.ar>
2025-07-22 22:26:37 -0300

    Se hicieron cambios en el header

--- Sistema-INDEC/frontend/indec/src/app/main/pages/home/home.component.ts
+++ Sistema-INDEC/frontend/indec/src/app/main/pages/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../../core/header/header.component';
+import { HeaderComponent } from '../../../core/components/header/header.component';
+import { BuscadorComponent } from '../../../core/components/buscador/buscador.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, BuscadorComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
 export class HomeComponent {
 
 }
