b5b5541fd5edaa6c2eee673fe47dee930771f87a
Roberto Gomez <rgomezmartearena@ubp.edu.ar>
2025-07-23 20:34:19 -0300

    Se diseño el filtro para la sucursal

--- Sistema-INDEC/frontend/indec/src/app/core/components/filtro-localidad/filtro-localidad.component.ts
+++ Sistema-INDEC/frontend/indec/src/app/core/components/filtro-localidad/filtro-localidad.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IProvincias } from '../../../main/api/models/i-provincias';
+import { SucursalesService } from '../../../main/api/resources/sucursales.service';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-filtro-localidad',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './filtro-localidad.component.html',
   styleUrl: './filtro-localidad.component.css'
 })
-export class FiltroLocalidadComponent {
+export class FiltroLocalidadComponent implements OnInit {
+  provincias: IProvincias[] = [];
+  provincia = new FormControl('');
+  localidades = new FormControl('');
 
+  constructor(private sucursalesServices: SucursalesService) {}
+
+  ngOnInit(): void {
+    this.sucursalesServices.obtenerProvincias().subscribe((data) => {
+      this.provincias = data;
+    });
+  }
 }
