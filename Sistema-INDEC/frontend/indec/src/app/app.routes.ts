705ce1dc46d02d2d3c2d96fef88e09eed0a545fe
Roberto Gomez <rgomezmartearena@ubp.edu.ar>
2025-07-22 22:27:21 -0300

    Se definieron rutas

--- Sistema-INDEC/frontend/indec/src/app/app.routes.ts
+++ Sistema-INDEC/frontend/indec/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './main/pages/home/home.component';
+import { SucursalesComponent } from './main/pages/sucursales/sucursales/sucursales.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: HomeComponent },
+  { path: 'inicio/sucursales', component: SucursalesComponent },
+];
