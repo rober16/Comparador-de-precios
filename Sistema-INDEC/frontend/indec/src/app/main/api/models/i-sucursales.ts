b834657df5185cfdef1863e130e50b12400a2978
Roberto Gomez <rgomezmartearena@ubp.edu.ar>
2025-07-29 22:01:52 -0300

    Se creo la interfaz para la sucursal

--- Sistema-INDEC/frontend/indec/src/app/main/api/models/i-sucursales.ts
+++ Sistema-INDEC/frontend/indec/src/app/main/api/models/i-sucursales.ts
@@ -1,2 +1,12 @@
 export interface ISucursales {
+    nroSucursal: number;
+    nomSucursal: string;
+    calle: string;
+    nroCalle: number;
+    telefonos: string;
+    coordLatitud: string;
+    coordLongitud: string;
+    horarioSucursal: string;
+    serviciosDisponibles: string;
+    nroLocalidad: number;
 }
